4853c50df310b738d75e87c69133acb0cbbdee31
Hibanaw Hu <hibanaw@qq.com>
2023-01-15 21:46:52 +0800

    modulize

--- src/listener.h
+++ src/listener.h
@@ -3,16 +3,19 @@
 
 #include<bios.h>
 #include<string.h>
+#include<string.h>
 #include"mouse.h"
 #include"ime.h"
 #include"log.h"
 #include"keyboard.h"
 #include"core.h"
+#include"module.h"
+
 
 int listenerHome(Mouse*);
-int listenerLogin(struct Textbox *bu, struct Textbox *bp, Mouse*);
-int listenerButton(int x, int y, Mouse m, char* text = NULL, int sizex = 100, int sizey = 30);
-int listenerTextbox(Textbox *b);
+int listenerLogin(struct Textbox *bu, struct Textbox *bp,  Button *b, Mouse *m);
+int listenerButton( Button *b, Mouse m);
+int listenerTextbox(struct Textbox *b);
 int listenerKey();
 
 #endif
