abb5b918c8f5ea11f1994972279ab5fee64f9658
Hibanaw Hu <hibanaw@qq.com>
2023-02-22 23:26:53 +0800

    Add main.

---
+++ src/main.c
@@ -0,0 +1,18 @@
+/**
+ * @file main.c
+ * @author Hibanaw Hu (hibanaw@qq.com)
+ * @brief 
+ * @date 2023-02-12
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include "main.h"
+
+int main(){
+	init();
+	Homepage();
+	
+	return 0;
+}
---
+++ src/main.h
@@ -0,0 +1,16 @@
+/**
+ * @file main.h
+ * @author Hibanaw Hu (hibanaw@qq.com)
+ * @brief 
+ * @date 2023-02-12
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+#include "init.h"
+#include "homepage.h"
+
+#endif
