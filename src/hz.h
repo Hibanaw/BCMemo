4791b2c812c5dce1181db6a5e28c91bcfa631e84
Hibanaw Hu <hibanaw@qq.com>
2023-03-15 23:40:21 +0800

    Use hz.h.

---
+++ src/hz.h
@@ -0,0 +1,12 @@
+#ifndef __HZ_H__
+#define __HZ_H__
+
+#include <graphics.h>
+#include<conio.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+void hz_puthz(char *s, int x, int y, int flag,int part,int color);
+#endif
+
