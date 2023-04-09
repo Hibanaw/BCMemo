87c0ee25b51f482027d9e7218cedb5fc316bf8c9
Hibanaw Hu <hibanaw@qq.com>
2023-04-09 23:10:13 +0800

    Update file.

--- src/file.c
+++ src/file.c
@@ -1,18 +1,15 @@
 /**
  * @file file.c
- * @author wywgwt (2504133124@qq.com)
+ * @author wywgwt (2504133124@qq.com), Hibanaw Hu (hibanaw@qq.com)
  * @brief 
  * @date 2023-04-06
  * 
  * @copyright Copyright (c) 2023
  * 
  */
-#ifndef _WRITE_H_
-#define _WRITE_H_
-#include<stdio.h>
-#include<string.h>
-#include"memos.h" 
-#include"memo.h"
+
+#include "file.h"
+
 FILE **memofile_current(){
 	static FILE *f;
 	return &f;
@@ -20,14 +17,14 @@ FILE **memofile_current(){
 
 void memofile_write(char* filePath, Memo *m){
 	FILE **fp = memofile_current();
-	if ((*fp=fopen(filePath,"w"))==NULL)
+	MemoBlock *h=m->head;
+	MemoBlock *p;
+	if ((*fp=fopen(filePath,"wb"))==NULL)
 	{
 	  printf("the file can not be open!");
 	  getch();
 	  exit(1);
 	}
-	MemoBlock *h=m->head;
-	MemoBlock *p;
 	p=h;
 	if(h==NULL)
 	{
@@ -36,7 +33,7 @@ void memofile_write(char* filePath, Memo *m){
 		exit(1);
 	}
 	else{
-		while((p->next)!=NULL)
+		while(p!=NULL)
 		{
 			memofile_writeBlock(p);
 			p=p->next;
@@ -47,73 +44,40 @@ void memofile_write(char* filePath, Memo *m){
 
 void memofile_writeBlock(MemoBlock *p)
 {
-	FILE **fp = memofile_current();
-	if(p->type==IMAGE)
-	{
-		fprintf(*fp,"%s %c %s\n",p->lastEditUser,'I',p->content);
-	}
-	if(p->type==PARAGRAPH)
-	{
-		fprintf(*fp, "%s %c %s\n",p->lastEditUser,'p',p->content);
-	}
-	if(p->content==CHECKBOX)
-	{
-		fputc('C',*fp);
-		fprintf(*fp,"%d",p->checkBoxisChecked);
-		fprintf(*fp,"%s %c %d %s\n",p->lastEditUser,'C',p->checkBoxChecked,p->content);
-	}
+	FILE *fp = *memofile_current();
+	fwrite(p, sizeof(MemoBlock), 1, fp);
 }
-void memofile_readBlock(MemoBlock *p)
+MemoBlock *memofile_readBlock()
 {
-	FILE **fp=memofile_current();
-	if(feof(*fp)) return;
-	p=(MemoBlock *)malloc(sizeof(MemoBlock)); 
-	char t = fgetc(*fp);
-	switch(t)
-	{
-	case 'I':
-		fscanf(*fp,"%s %c ",&(p->lastEditUser), &(p->type));
-		fgets(p->content,500,*fp);
-		break;
-	case 'P':
-		p->type=PARAGRAPH;
-		fgets(p->content,500,*fp);
-		fscanf(*fp,"%s %c",&(p->lastEditUser),&(p->type));
-		fgets(p->content,500,*fp);
-		break;
-	case 'C':
-		fscanf(*fp,"%s %c %d",&(p->lastEditUser),&(p->checkBoxisChecked),&(p->type));
-		fgets(p->content,500,*fp);
-		break;
-	default:
-	break;
-    }
+	FILE *fp=*memofile_current();
+	char t;
+	MemoBlock *p;
+	if(feof(fp)){
+		return NULL;
+	}
+	p=(MemoBlock *)malloc(sizeof(MemoBlock));
+	fread(p, sizeof(MemoBlock), 1, fp);
+	return p;
  } 
  
- void memofile_read(Memo *m,char *filePath)
- {
- 	FILE **fp=memofile_current();
- 	if ((*fp=fopen(filePath,"r"))==NULL)
+Memo memofile_read(char *filePath)
+{
+	FILE **fp = memofile_current();
+	Memo m;
+	MemoBlock *p;
+
+ 	if ((*fp=fopen(filePath,"rb"))==NULL)
 	{
-	  printf("the file can not be open!");
-	  getch();
-	  exit(1);
-	  
+		*fp = fopen(filePath, "wb");
+		fclose(fp);
+		*fp=fopen(filePath,"rb");
 	}
-	MemoBlock *h=m->head;
-	MemoBlock *p=h;
-	while(1)
+	m.head = p = memofile_readBlock();
+	while(p)
 	{
-		if(p==NULL)
-		{ 
-			break;
-		}
-		else
-		{
-			memofile_readBlock(p);
-			p=p->next;
-		}
+		p->next = memofile_readBlock();
+		p = p->next;
 	}
 	fclose(*fp);
- }
-#endif
+	return m;
+}
--- src/file.h
+++ src/file.h
@@ -7,20 +7,22 @@
  * @copyright Copyright (c) 2023
  * 
  */
-#ifndef _WRITE_H_
-#define _WRITE_H_
+
+#ifndef __FILE_H__
+#define __FILE_H__
 #include<stdio.h>
 #include<string.h>
 #include"memos.h" 
 #include"memo.h"
+
 FILE **memofile_current();
 
 void memofile_write(char* filePath, Memo *m);
 
 void memofile_writeBlock(MemoBlock *p);
 
-void memofile_readBlock(MemoBlock *p);
+MemoBlock *memofile_readBlock();
 
- void memofile_read(Memo *m,char *filePath);
+Memo memofile_read(char *filePath);
  
 #endif
