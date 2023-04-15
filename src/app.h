3f7bd90b6eed7f80a43e3774a0fb87aa9c65d852
耀charming\耀 <2504133124@qq.com>
2023-04-15 09:59:53 +0800

    add current_editor

--- src/app.h
+++ src/app.h
@@ -21,7 +21,7 @@
 #include "anim.h"
 #include "meditor.h"
 #include "router.h"
-
+#include"userpage.h"
 enum{
     AppRouterExpand = 1,
     AppExit
@@ -29,11 +29,13 @@ enum{
 
 typedef struct
 {
-    char *uid;
+    char uid[6][20];
+    int current_editor;
 }AppData;
 
 AppData *appData();
 
 void app();
 
+void sidebar_draw();
 #endif
